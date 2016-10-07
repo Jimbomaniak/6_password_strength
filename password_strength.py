67abd6e1ddb1667069752846a712eed82a1ad06a
Olegka <jimboman54321@gmail.com>
2016-10-07 17:18:32 +0300

    function get black list, remove comments

--- password_strength.py
+++ password_strength.py
@@ -1,36 +1,46 @@
 import re
 import requests
 
-
-def get_password_strength(password):
+def get_black_list_passwords():
     URL = 'https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/10_million_password_list_top_1000.txt'
-    pass_base_get = requests.get(URL)
-    pass_base_list = pass_base_get.text.split('\n')
+    black_list_get = requests.get(URL)
+    black_list_passwords = black_list_get.text.split('\n')
+    return black_list_passwords
+
+
+def get_password_strength(password, black_list_passwords=None):
     hardness = 0
-    WORST_LEN, BAD_LEN, GOOD_LEN, BEST_LEN = (3, 6, 8, 10)
-    if password not in pass_base_list:
+    worst_len, bad_len, good_len, best_len = (3, 6, 8, 10)
+    has_digits = re.search(r'\d+', password)
+    has_small_letters = re.search(r'[a-z]', password)
+    has_big_letters = re.search(r'[A-Z]', password)
+    has_whitespaces = re.search(r'\s', password)
+    has_other_symbols = re.search(r'[^A-Za-z\s\w]', password)
+    len_pass = len(password)
+    if password not in black_list_passwords:
         hardness += 1
-    if len(password) > WORST_LEN:
+    if len_pass > worst_len:
         hardness += 1
-    if len(password) > BAD_LEN:
+    if len_pass > bad_len:
         hardness += 1
-    if len(password) > GOOD_LEN:
+    if len_pass > good_len:
         hardness += 1
-    if len(password) > BEST_LEN:
+    if len_pass > best_len:
         hardness += 1
-    if re.search(r'\d+', password):  #has digits
+    if has_digits:
         hardness += 1
-    if re.search(r'[a-z]', password):  #has small letters
+    if has_small_letters:
         hardness += 1
-    if re.search(r'[A-Z]', password):  #has big letters
+    if has_big_letters:
         hardness += 1
-    if re.search(r'\s', password):  #has whitespaces
+    if has_whitespaces:
         hardness += 1
-    if re.search(r'[^A-Za-z\s\w]', password):  #has other symbols
-        hardness +=1
+    if has_other_symbols:
+        hardness += 1
     return hardness
 
 if __name__ == '__main__':
     password = input('enter your password: ')
-    get_hardness = get_password_strength(password)
-    print('Hardness of your password is {0}'.format(get_hardness))
+    black_list_passwords = get_black_list_passwords()
+    password_strength = get_password_strength(password,black_list_passwords)
+    print('Hardness of your password is {0}'.format(password_strength))
